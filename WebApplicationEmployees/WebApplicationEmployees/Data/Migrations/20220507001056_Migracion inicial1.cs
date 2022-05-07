5b165d96b3bd46640f3b283e9a6b0664e5bd6a7a
MiguelCruz691 <92962710+MiguelCruz691@users.noreply.github.com>
2022-05-06 23:16:32 -0500

    Creacion del modelo Employee. Migraciones EF. Update-Database EF

---
+++ WebApplicationEmployees/WebApplicationEmployees/Data/Migrations/20220507001056_Migracion inicial1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationEmployees.Data.Migrations
+{
+    public partial class Migracioninicial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- WebApplicationEmployees/WebApplicationEmployees/Startup.cs
+++ WebApplicationEmployees/WebApplicationEmployees/Startup.cs
@@ -27,14 +27,17 @@ namespace WebApplicationEmployees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContextPool<ApplicationDbContext>(option =>
+            option.UseSqlServer(connectionString));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
